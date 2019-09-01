4dfec31493b8954054df39e948de4cafc8f16d1c
trustable-code <krauter.simon@arcor.de>
2019-09-01 16:36:07 +0200

    Extend example_06_keyboard_events

--- examples/example_06_keyboard_events.nim
+++ examples/example_06_keyboard_events.nim
@@ -6,16 +6,33 @@ app.init()
 
 var window = newWindow()
 
+var container = newLayoutContainer(Layout_Vertical)
+window.add(container)
+
+var textBox = newTextBox()
+container.add(textBox)
+
 var label = newLabel()
-window.add(label)
+
 
 window.onKeyDown = proc(event: KeyboardEvent) =
-  label.text = label.text & "KeyDown event: key: " & $event.key & ", unicode: " & $event.unicode & ", character: " & event.character & ", down keys: " & $downKeys() & "\n"
+  label.text = label.text & "Window KeyDown event: key: " & $event.key & ", unicode: " & $event.unicode & ", character: " & event.character & ", down keys: " & $downKeys() & "\n"
 
   # Ctrl + Q -> Quit application
   if Key_Q.isDown() and Key_ControlL.isDown():
     app.quit()
 
+textBox.onKeyDown = proc(event: KeyboardEvent) =
+  label.text = label.text & "TextBox KeyDown event: key: " & $event.key & ", unicode: " & $event.unicode & ", character: " & event.character & ", down keys: " & $downKeys() & "\n"
+
+  # Accept only digits
+  if event.character.len != 1 or event.character[0] notin '0'..'9':
+    event.handled = true
+
+
+container.add(label)
+
 window.show()
+textBox.focus()
 
 app.run()
